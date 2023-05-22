ee11303b2201addb4ecb2967f6285083a38fb859
Huiyu <liuhuiyu321@gmail.com>
2023-05-22 03:59:59 +0200

    add subscribe perceivedObjectContainer in car2x_unit

--- src/car2x_unit/src/car2x_unit.cpp
+++ src/car2x_unit/src/car2x_unit.cpp
@@ -2,7 +2,9 @@
 #include "std_msgs/String.h"
 #include "cpm_interface.pb.h"
 #include <boost/asio.hpp>
-#include "car2x_unit/CPMessage.h"
+#include <ros/message_operations.h>
+#include <sstream>
+#include "car2x_unit/PerceivedObjectContainer.h"
 
 using boost::asio::ip::udp;
 using namespace std;
@@ -11,21 +13,21 @@ class UDPListenerNode
 public:
     UDPListenerNode()
     {
-        ros::NodeHandle nh;                                          // 初始化节点句柄
-        publisher_ = nh.advertise<std_msgs::String>("udp_data", 10); // 定义一个发布者，主题名为"udp_data"
+        ros::NodeHandle nh;
+        publisher_ = nh.advertise<std_msgs::String>("udp_data", 10);
+        subscriber_ = nh.subscribe("collective_perception", 1000, &UDPListenerNode::collectivePerceptionCallback, this);
 
-        socket_ = std::make_unique<udp::socket>(io_context_, udp::endpoint(udp::v4(), 12345)); // 创建一个UDP套接字，绑定到端口12345
-
-        start_receive(); // 开始接收数据
+        socket_ = std::make_unique<udp::socket>(io_context_, udp::endpoint(udp::v4(), 12345));
+        start_receive();
         io_thread_ = std::make_unique<std::thread>([this]()
-                                                   { io_context_.run(); }); // 在新的线程中运行io_context_
+                                                   { io_context_.run(); });
     }
 
     ~UDPListenerNode()
     {
-        io_context_.stop(); // 停止io_context_
+        io_context_.stop();
         if (io_thread_ && io_thread_->joinable())
-            io_thread_->join(); // 等待线程结束
+            io_thread_->join();
     }
 
 private:
@@ -35,44 +37,56 @@ private:
             boost::asio::buffer(recv_buffer_), remote_endpoint_,
             boost::bind(&UDPListenerNode::handle_receive, this,
                         boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred)); // 异步接收数据，并绑定回调函数handle_receive
+                        boost::asio::placeholders::bytes_transferred));
+    }
+
+    void collectivePerceptionCallback(const car2x_unit::PerceivedObjectContainer::ConstPtr &msg)
+    {
+
+        std::ostringstream oss;
+
+        ros::message_operations::Printer<car2x_unit::PerceivedObjectContainer>::stream(oss, "", *msg);
+
+        ROS_INFO("----------------------------------------------------------------------------------\n");
+        ROS_INFO("%s", oss.str().c_str());
     }
 
     void handle_receive(const boost::system::error_code &error, std::size_t bytes_transferred)
     {
-        if (!error) // 如果没有错误
+        if (!error)
         {
             CPMessage message;
-            if (!message.ParseFromArray(recv_buffer_.data(), bytes_transferred)) // 解析接收到的数据
+            if (!message.ParseFromArray(recv_buffer_.data(), bytes_transferred))
             {
-                ROS_ERROR("Failed to parse received data"); // 如果解析失败，打印错误信息
+                ROS_ERROR("Failed to parse received data");
             }
             else
             {
                 std_msgs::String str_msg;
-                str_msg.data = message.DebugString(); // 将接收到的数据转为字符串
-                publisher_.publish(str_msg);          // 发布消息
+                str_msg.data = message.DebugString();
+                publisher_.publish(str_msg);
 
-                ROS_INFO("Received message: %s\n-----------------\n", message.DebugString().c_str()); // 打印接收到的消息
+                ROS_INFO("Received message: %s\n-----------------\n", message.DebugString().c_str());
             }
 
-            start_receive(); // 继续接收数据
+            start_receive();
         }
     }
 
 private:
-    ros::Publisher publisher_;               // 定义一个发布者
-    boost::asio::io_context io_context_;     // 定义一个io_context_
-    std::unique_ptr<udp::socket> socket_;    // 定义一个UDP套接字
-    udp::endpoint remote_endpoint_;          // 定义一个UDP端点，用于存储发送者的信息
-    std::array<char, 1024> recv_buffer_;     // 定义一个接收缓冲区
-    std::unique_ptr<std::thread> io_thread_; // 定义一个线程
+    ros::Publisher publisher_;
+    ros::Subscriber subscriber_;
+    boost::asio::io_context io_context_;
+    std::unique_ptr<udp::socket> socket_;
+    udp::endpoint remote_endpoint_;
+    std::array<char, 1024> recv_buffer_;
+    std::unique_ptr<std::thread> io_thread_;
 };
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "udp_listener"); // 初始化ROS节点
-    UDPListenerNode node;                  // 创建一个UDPListenerNode对象
-    ros::spin();                           // 开始ROS事件循环
+    ros::init(argc, argv, "udp_listener");
+    UDPListenerNode node;
+    ros::spin();
     return 0;
 }
